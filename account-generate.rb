5169000fe0d678968efbb6150748d924612c02dd
Ash Tyndall <git@atyndall.net>
2013-03-02 10:30:28 +0800

    Rewrite code to iterate

--- account-generate.rb
+++ account-generate.rb
@@ -3,5 +3,11 @@ require "bundler/setup"
 require "whois"
 
 c = Whois::Client.new(:host=>'whois.uwa.edu.au')
-r= c.query('Ash Tyndall in Full Name', false)
-puts r
+
+File.foreach('freshers.txt') { |l|
+  name = l.chomp
+
+  r = c.query("show people having '#{name}' as Full Name and Students as Department", false)
+  puts r
+}
+
