b040f5a071040af120d3db1c66769507a05898db
Ash Tyndall <git@atyndall.net>
2013-03-07 11:52:19 +0800

    New implementation using regexes and sockets

--- account-generate.rb
+++ account-generate.rb
@@ -1,15 +1,39 @@
-require "rubygems"
-require "bundler/setup"
-require "whois"
+require 'rubygems'
+require 'bundler/setup'
+require 'socket'
 
-c = Whois::Client.new(:host=>'whois.uwa.edu.au')
+puts "Writing results to results.txt"
+File.open('results.txt', 'w') do |r|
+  File.foreach('students.txt') do |n|
+    name = n.chomp
 
-File.foreach('freshers.txt') { |l|
-  name = l.chomp
+    s = TCPSocket.new 'whois.uwa.edu.au', 43
+    s.puts "show people having #{name} as name and Students as Department\r\n"
 
-  query = "show people having '#{name}' as name and Students as Department"
-  puts query
-  r = c.query(query, false)
-  puts r
-}
+    found = false
+    while l = s.gets
+      if m = l.match(/No matches were found/)
+        puts "Could not resolve #{name}"
+        r.puts "#{name}:?"
+        found = true
+        break
+      end
+
+      if m = l.match(/E-mail:\s*([0-9]{8})@student.uwa.edu.au/)
+        num = m.captures[0]
+        puts "Resolved #{name} to #{num}"
+        r.puts "#{name}:#{num}"
+        found = true
+      end
+    end
+
+    unless found
+      puts "Encountered strange output for #{name}"
+      r.puts "#{name}:??"
+    end
+
+    s.close
+    sleep 1
+  end
+end
 
